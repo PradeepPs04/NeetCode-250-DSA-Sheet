8207610b6dcca00725ff9264e4c954b5327be436
PradeepPs04 <pradeep.ps2004@gmail.com>
2025-09-01 16:28:11 +0530

    add: graph 1 question

---
+++ 11 Graph/14_graph_valid_tree.cpp
@@ -0,0 +1,12 @@
+/*
+    https://neetcode.io/problems/valid-tree?list=neetcode250
+
+    Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
+
+    n = 5
+    edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
+
+    Output:
+    true
+*/
+
