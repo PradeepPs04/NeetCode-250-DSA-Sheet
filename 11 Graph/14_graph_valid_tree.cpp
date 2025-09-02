006aabc44c33f5506421d32add8f670463a7b96b
PradeepPs04 <pradeep.ps2004@gmail.com>
2025-09-02 17:17:55 +0530

    add: graph 1 question

--- 11 Graph/14_graph_valid_tree.cpp
+++ 11 Graph/14_graph_valid_tree.cpp
@@ -10,3 +10,43 @@
     true
 */
 
+class Solution {
+private:
+    bool dfs(int node, int prev, vector<int>adj[], vector<int>& visited) {
+        visited[node] = 1;
+
+        for(auto it:adj[node]) {
+            if(!visited[it]) {
+                if(dfs(it, node, adj, visited)) return true;
+            }
+            else if(it != prev)   return true;
+        }
+
+        return false;
+    }
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        vector<int>adj[n];
+
+        for(auto it:edges) {
+            int u = it[0];
+            int v = it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int>visited(n, 0);
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                cnt++;
+                if(dfs(i, -1, adj, visited))   return false;
+            }
+        }
+
+        // more than 1 connected compoenents
+        if(cnt > 1) return false;
+
+        return true;
+    }
+};
